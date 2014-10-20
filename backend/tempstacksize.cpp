f9703f81a0f291e3f002d18809010fabc9fca8a1
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2014-10-20 11:42:22 -0700

    Added some code for generating metadata

--- backend/tempstacksize.cpp
+++ backend/tempstacksize.cpp
@@ -1,5 +1,9 @@
 #include "tempstacksize.h"
 
+#include <common/llvm_warnings_push.h>
+#include <llvm/IR/Constants.h>
+#include <common/llvm_warnings_pop.h>
+
 static llvm::RegisterPass<TemporaryStackSize> X("TemporaryStackSize", "Calculate the temporaries used in function", false, false);
 
 char TemporaryStackSize::ID = 0;
@@ -8,6 +12,7 @@ bool TemporaryStackSize::runOnFunction(llvm::Function &F)
 {
     unsigned int numTemporaries = 0;
 
+    llvm::Instruction* pII = nullptr;
     // Loop through all the functions and figure out how many temporaries we would need
     for (llvm::Function::iterator bb = F.begin(), bbend = F.end(); bb != bbend; ++bb)
     {
@@ -16,11 +21,28 @@ bool TemporaryStackSize::runOnFunction(llvm::Function &F)
         {
             if (i->getNumUses() > 0)
                 ++numTemporaries;
+            pII = i;
         }
     }
 
     m_numTemporaries[&F] = numTemporaries;
 
+    /* Experiment with Metadata 
+    // Add a few metadata
+    llvm::Module *pM = F.getParent();
+    llvm::NamedMDNode* pNamedNode = pM->getOrInsertNamedMetadata("buffer_flags");
+    llvm::MDString* pS = llvm::MDString::get(pM->getContext(), "my_named_string_node");
+    std::vector<llvm::Value*> nodes;
+    nodes.push_back(pS);
+    llvm::ConstantInt* pCI= llvm::ConstantInt::get(llvm::Type::getInt32Ty(pM->getContext()), 2);
+    nodes.push_back(pCI);
+    llvm::MDNode* pMDNode = llvm::MDNode::get(pM->getContext(), nodes);
+    pNamedNode->addOperand(pMDNode);
+
+    pII->setMetadata("buffer_type", pMDNode);
+
+    pM->dump();
+    */
     return false;
 }
 
